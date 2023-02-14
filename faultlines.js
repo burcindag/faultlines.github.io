20036a0d2958c36941ff3e7a48a9169106c0491e
bdagistan <66089529+burcindag@users.noreply.github.com>
2023-02-14 18:01:57 +0300

    Update faultlines.js

--- faultlines.js
+++ faultlines.js
@@ -5,26 +5,8 @@ function initMap() {
     zoom: 7,
     center: { lat: 38.9637, lng: 35.2433 },
   });
-/*
-  // Load fault line coordinates directly from text file
-  fetch('faults.txt')
-    .then(response => response.text())
-    .then(text => {
-      var lines = text.split('\n');
 
-      // Draw fault lines
-      for (var i = 0; i < lines.length; i++) {
-        var line = new google.maps.Polyline({
-          path: google.maps.geometry.encoding.decodePath(lines[i]),
-          strokeColor: 'red',
-          strokeOpacity: 1.0,
-          strokeWeight: 2
-        });
-        line.setMap(map);
-      }
-    });
-  */
-    var url = "https://burcindagistan.com/faultlines.github.io/data/faylar.kml";
+    var url = "./data/faylar.kml";
   
     var gridLayer = new google.maps.KmlLayer(
     {
