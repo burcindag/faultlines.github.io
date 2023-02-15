6da066a2c55b8f8b084feced4ae4f31395f4a01c
bdagistan <66089529+burcindag@users.noreply.github.com>
2023-02-15 09:36:07 +0300

    Update faultlines.js
    disable streetview control

--- faultlines.js
+++ faultlines.js
@@ -4,6 +4,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
     center: { lat: 38.9637, lng: 35.2433 },
+    streetViewControl: false
   });
 
     var url = "https://burcindagistan.com/faultlines.github.io/data/faultlines.kml";
