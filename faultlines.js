1c64e8c4f4cacc63e61916414ca792c3c2af5270
bdagistan <66089529+burcindag@users.noreply.github.com>
2023-02-15 08:53:29 +0300

    Update faultlines.js

--- faultlines.js
+++ faultlines.js
@@ -17,3 +17,29 @@ function initMap() {
        clickable : true
     });
 }
+function myLocation() {
+  // Create a new map centered on your current location
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var myLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myLocation,
+      zoom: 15
+    });
+    // Add a marker at your current location
+    var marker = new google.maps.Marker({
+      position: myLocation,
+      map: map
+    });
+  });
+  
+   var url = "https://burcindagistan.com/faultlines.github.io/data/faultlines.kml";
+  
+    var faultlinesLayer = new google.maps.KmlLayer(
+    {
+       url: url,
+       suppressInfoWindows: true,  
+       map:map,
+       zindex: 0,
+       clickable : true
+    });
+}
